a0d4f969bf01f70d51dbef7837d6b3559b31011d
JPillS <jason.pilla@student.sl.on.ca>
2025-10-11 13:55:49 -0400

    Added QuantityCounter

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,9 +1,11 @@
+import QuantityCounter from "./QuantityCounter";
+
 export default function ProductCard({productName, image, brand, price}) {
   return <div className="ProductCard">
     <h3>{productName}</h3>
     <img src={image} alt={productName + brand} />
     <p>{brand}</p>
-    <div className="ProductQuantityDiv">{/*counter*/}</div>
+    <QuantityCounter />
     <h3>{price}</h3>
     <button>Add to Cart</button>
   </div>;
--- src/components/QuantityCounter.jsx
+++ src/components/QuantityCounter.jsx
@@ -1,5 +1,17 @@
-function QuantityCounter() {
-  return <>{/* Add your code here! */}</>;
-}
+import { useState } from "react"; 
 
-export default QuantityCounter;
+export default function QuantityCounter() {
+  const minQuantity = 0;
+  const maxQuantity = 10;
+  const [quantity, setQuantity] = useState(0);
+
+  return <div className="ProductQuantityDiv">
+    <button onClick={() =>
+    quantity > minQuantity &&
+    setQuantity(quantity - 1)}>-</button>
+    <p>{quantity}</p>
+    <button onClick={() =>
+    quantity < maxQuantity &&
+    setQuantity(quantity + 1)}>+</button>
+  </div>;
+}
