a85b0f372d87015f20050651ac41ace446d4ec09
JPillS <jason.pilla@student.sl.on.ca>
2025-10-12 17:15:21 -0400

    Finished NavBar.

--- src/components/GrociariesAppContainer.jsx
+++ src/components/GrociariesAppContainer.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import products from "../data/products";
 
 import NavBar from "./NavBar";
@@ -5,10 +7,26 @@ import CartContainer from "./CartContainer";
 import ProductsContainer from "./ProductsContainer";
 
 export default function GrociariesAppContainer() {
+  const [cartItems, setCartItems] = useState([]);
+  const [counter, setCounter] = useState(0);
+  let cartItemsCopy = [...cartItems];
+
+  const addToCart = () => {
+    setCounter(counter + 5);
+    cartItemsCopy.push(counter);
+    setCartItems(cartItemsCopy);
+  }
+  const removeFromCart = () => {
+    cartItemsCopy.pop();
+    setCartItems(cartItemsCopy);
+  }
   return <>
-  <NavBar />
+  <NavBar hasItems={cartItems.length > 0}/>
   <div className="GroceriesApp-Container">
     <ProductsContainer products={products} />
     <CartContainer />
+    <p>{cartItems[cartItems.length - 1]}</p>
+    <button onClick={() => addToCart()}>+</button>
+    <button onClick={() => removeFromCart()}>-</button>
   </div></>;
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,10 @@
 import cartEmpty from "../assets/cart-empty.png";
-import cartFull from "../assets/cart-empty.png";
+import cartFull from "../assets/cart-full.png";
 
-export default function NavBar() {
-
+export default function NavBar({hasItems}) {
   return <div className="NavBar">
       <h3 className="NavUser">Hello, username</h3>
       <h2 className="NavTitle">Grocieries App 🍎</h2>
-      <img className="NavCart" src={cartEmpty} />
+      <img className="NavCart" src={hasItems ? cartFull : cartEmpty} />
   </div>;
 }
