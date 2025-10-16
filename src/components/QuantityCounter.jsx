1d85f24145982cc92c404c2d93457264e3673005
JPillS <jason.pilla@student.sl.on.ca>
2025-10-16 13:35:09 -0400

    Format change.

--- src/components/QuantityCounter.jsx
+++ src/components/QuantityCounter.jsx
@@ -7,11 +7,12 @@ export default function QuantityCounter({
   minQuantity = 0
 }) {
   return <div className="ProductQuantityDiv">
-    <button onClick={() =>
-    quantity > minQuantity &&
-    setQuantity(id, -1)}>-</button>
+    <button onClick={
+      () => quantity > minQuantity && setQuantity(id, -1)
+    }>-</button>
     <p>{quantity}</p>
-    <button onClick={() =>
-    setQuantity(id, 1)}>+</button>
+    <button onClick={
+      () => setQuantity(id, 1)
+    }>+</button>
   </div>;
 }
