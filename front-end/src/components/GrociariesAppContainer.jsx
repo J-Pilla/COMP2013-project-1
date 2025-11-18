8d0d0316749cb9f8553046817ad0dd9090652401
J-Pilla <pilla.jason@gmail.com>
2025-11-18 16:02:14 -0500

    Refactored to use a database and made quantity part of the products state.

--- front-end/src/components/GrociariesAppContainer.jsx
+++ front-end/src/components/GrociariesAppContainer.jsx
@@ -1,103 +1,133 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
-import products from "../data/products";
-
-import NavBar from "./NavBar";
+import NavBar from "./r";
 import CartContainer from "./CartContainer";
 import ProductsContainer from "./ProductsContainer";
 
 export default function GrociariesAppContainer() {
-  // an array of objects that contain a product id and a quantity
-  const [productQuantities, setProductQuantities] = useState(products.map((product) => {
-    return {
-      id: product.id,
-      quantity: 0
-    };
-  }));
+	// URL
+	const baseURL = "http://localhost:3000/";
+	const productsPath = "products/";
 
-  // set one quantity in productQuantities
-  const setProductQuantity = (id, quantity) => {
-    const newProductQuantities = productQuantities.map((productQuantity) => {
-      let newProductQuantity = {...productQuantity};
-      
-      if (productQuantity.id === id)
-        newProductQuantity.quantity += quantity;
+	// default states
 
-      return newProductQuantity;
-    });
+	// states
+	// quantity is now attatched to products
+	const [products, setProducts] = useState([]);
+	// an array populated by ProcuctCard, each index contains a product, quantity, and totalPrice
+	const [cartItems, setCartItems] = useState([]);
 
-    setProductQuantities(newProductQuantities);
-  };
+	// useEffect
+	useEffect(() => {
+		fetchProducts();
+	}, []);
 
-  /* an array populated by ProcuctCard,
-   * each index contains a product, quantity, and totalPrice */
-  const [cartItems, setCartItems] = useState([]);
+	const fetchProducts = async () => {
+		try {
+			const response = await axios.get(baseURL + productsPath);
+			setProducts(
+				response.data.map((data) => {
+					const { id, ...product } = data;
+					return {
+						...product,
+						quantity: 0,
+					};
+				})
+			);
+		} catch (error) {
+			console.log(error.message);
+		}
+	};
 
-  // adds a cartItem via ProductCard
-  const addToCart = (id, quantity) => {
-    let cartId = cartItems.find((cartItem) => cartItem.product.id === id);
-    
-    if (cartId === undefined) {
-      let newCartItems = [...cartItems];
-      const addedProduct = products.find((product) => product.id === id);
-      
-      newCartItems.push({
-        product: {...addedProduct},
-        quantity: quantity,
-        totalPrice: quantity * addedProduct.price.replace("$", "")
-      });
-      
-      setCartItems(newCartItems);
-    }
-    else {
-      setItemQuantity(id, quantity);
-    }
-  }
+	// set one quantity in products
+	const setProductQuantity = (_id, quantity) => {
+		console.log(_id);
+		const newProducts = products.map((product) => {
+			let newProduct = { ...product };
 
-  /* sets a cartItem's quantity if a cartItem is being added to,
-   * either via ProductCard or the QuantityCounter in CartCard,
-   * totalPrice is updated to reflect the quantity */
-  const setItemQuantity = (id, quantity) => {
-    const newCartItems = cartItems.map((cartItem) => {
-      let newCartItem = {...cartItem};
+			if (product._id === _id) {
+				console.log(newProduct);
+				newProduct.quantity += quantity;
+				console.log(newProduct);
+			}
 
-      if (cartItem.product.id === id)
-      {
-        newCartItem.quantity += quantity;
-        newCartItem.totalPrice += quantity * cartItem.product.price.replace("$", "")
-      }
+			return newProduct;
+		});
 
-      return newCartItem;
-    });
+		setProducts(newProducts);
+	};
 
-    setCartItems(newCartItems);
-  }
+	// adds a cartItem via ProductCard
+	const addToCart = (id, quantity) => {
+		let cartId = cartItems.find((cartItem) => cartItem.product.id === id);
 
-  // removes a cartItem via CartCard
-  const removeFromCart = (id) => {
-    const newCartItems = cartItems.filter((cartItem) => cartItem.product.id !== id);
-    setCartItems(newCartItems);
-  }
+		if (cartId === undefined) {
+			let newCartItems = [...cartItems];
+			const addedProduct = products.find((product) => product.id === id);
 
-  // resets cartItems[] via CartContainer
-  const emptyCart = () => {
-    setCartItems([]);
-  }
+			newCartItems.push({
+				product: { ...addedProduct },
+				quantity: quantity,
+				totalPrice: quantity * addedProduct.price.replace("$", ""),
+			});
 
-  return <>
-  {/* hasItems determintes if the cart shows up or not,
-    * I figured this woud be the easiest way to set up a ternary */}
-  <NavBar hasItems={cartItems.length > 0}/>
-  <div className="GroceriesApp-Container">
-    <ProductsContainer
-      products={products}
-      productQuantities={productQuantities}
-      setProductQuantity={setProductQuantity}
-      addToCart={addToCart}/>
-    <CartContainer
-      cartItems={cartItems}
-      setItemQuantity={setItemQuantity}
-      removeFromCart={removeFromCart}
-      emptyCart={emptyCart}/>
-  </div></>;
+			setCartItems(newCartItems);
+		} else {
+			setItemQuantity(id, quantity);
+		}
+	};
+
+	/* sets a cartItem's quantity if a cartItem is being added to,
+	 * either via ProductCard or the QuantityCounter in CartCard,
+	 * totalPrice is updated to reflect the quantity */
+	const setItemQuantity = (id, quantity) => {
+		const newCartItems = cartItems.map((cartItem) => {
+			let newCartItem = { ...cartItem };
+
+			if (cartItem.product.id === id) {
+				newCartItem.quantity += quantity;
+				newCartItem.totalPrice +=
+					quantity * cartItem.product.price.replace("$", "");
+			}
+
+			return newCartItem;
+		});
+
+		setCartItems(newCartItems);
+	};
+
+	// removes a cartItem via CartCard
+	const removeFromCart = (id) => {
+		const newCartItems = cartItems.filter(
+			(cartItem) => cartItem.product.id !== id
+		);
+		setCartItems(newCartItems);
+	};
+
+	// resets cartItems[] via CartContainer
+	const emptyCart = () => {
+		setCartItems([]);
+	};
+
+	return (
+		<>
+			{/* hasItems determintes if the cart shows up or not,
+			 * I figured this woud be the easiest way to set up a ternary */}
+			<NavBar hasItems={cartItems.length > 0} />
+			<div className="GroceriesApp-Container">
+				<ProductsContainer
+					products={products}
+					setProductQuantity={setProductQuantity}
+					addToCart={addToCart}
+				/>
+				<CartContainer
+					cartItems={cartItems}
+					setItemQuantity={setItemQuantity}
+					removeFromCart={removeFromCart}
+					emptyCart={emptyCart}
+				/>
+			</div>
+		</>
+	);
 }
